b47c602f3fe4fe3f0fc1d24bafeafe88c97cd154
Tobias Stamann <tobias.stamann@holisticon.de>
2016-03-06 00:46:48 +0100

    Make tracee context propagation on Jms side work

--- spring-boot/spring-boot-jms/src/main/java/io/tracee/examples/microservicedemo/jms/JmsMessageConsumer.java
+++ spring-boot/spring-boot-jms/src/main/java/io/tracee/examples/microservicedemo/jms/JmsMessageConsumer.java
@@ -1,19 +1,64 @@
 package io.tracee.examples.microservicedemo.jms;
 
+import io.tracee.Tracee;
+import io.tracee.TraceeBackend;
+import io.tracee.TraceeConstants;
+import io.tracee.binding.jms.TraceeMessageListener;
 import io.tracee.examples.microservicedemo.jms.config.JmsConfig;
 import org.slf4j.Logger;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.Map;
+
+import static io.tracee.configuration.TraceeFilterConfiguration.Channel.AsyncProcess;
+
 
 @Component
 public class JmsMessageConsumer {
 
-    @JmsListener(destination = "queue://" + JmsConfig.QUEUE)
-    public void receiveOrder(String calculation) {
+    @JmsListener(destination = JmsConfig.QUEUE) // "queue://" +
+    public void receiveOrder(String calculation, Message message) {
         Logger logger = org.slf4j.LoggerFactory.getLogger(JmsMessageProducerService.class);
-        logger.info("Do asynchronous calculation of {} , ", calculation);
+
+
+        try {
+            beforeProcessing(message);
+
+            logger.info("Do asynchronous calculation of {} - {}", calculation, message.getJMSMessageID());
+
+
+
+        } catch (JMSException e) {
+            logger.error("JMS processing error", e);
+        } finally {
+            cleanUp();
+        }
         //storeService.registerOrder(order);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public void beforeProcessing(final Message message) throws JMSException {
+        TraceeBackend backend = Tracee.getBackend();
+        if (backend.getConfiguration().shouldProcessContext(AsyncProcess)) {
+            final Object encodedTraceeContext = message.getObjectProperty(TraceeConstants.TPIC_HEADER);
+            if (encodedTraceeContext != null) {
+                final Map<String, String> contextFromMessage = (Map<String, String>) encodedTraceeContext;
+                backend.putAll(backend.getConfiguration().filterDeniedParams(contextFromMessage, AsyncProcess));
+            }
+        }
+
+    }
+
+    void cleanUp() {
+        TraceeBackend backend = Tracee.getBackend();
+
+        if (backend.getConfiguration().shouldProcessContext(AsyncProcess)) {
+            backend.clear();
+        }
     }
 
 }
--- spring-boot/spring-boot-jms/src/main/java/io/tracee/examples/microservicedemo/jms/config/JmsConfig.java
+++ spring-boot/spring-boot-jms/src/main/java/io/tracee/examples/microservicedemo/jms/config/JmsConfig.java
@@ -4,11 +4,12 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
-
+@EnableJms
 @Configuration
 public class JmsConfig {
 
